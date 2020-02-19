a754338e149f827a7767929e34a91fac049bb5ea
minjun_kim_mj <rlaalswns321@naver.com>
2020-02-20 01:13:20 +0900

    react client 추가 및 서브스크립스 추가

--- chat-room/src/server.js
+++ chat-room/src/server.js
@@ -1,19 +1,62 @@
-import { GraphQLServer } from "graphql-yoga";
-// import schema from "./schema";
-// import "./env";
+import { GraphQLServer, PubSub } from "graphql-yoga";
+const pubsub = new PubSub();
+const NEW_CHAT = "NEW_CHAT";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
+let chattingLog = [{
+  id: 0,
+  writer: "admin",
+  description: "HELLO"
+}];
+//기능별 분류 필요
+const typeDefs = ` 
+type Chat {
+  id: Int!
+  writer: String!
+  description: String!
+}
+type Query {
+  chatting: [Chat]!
+}
+type Mutation {
+  write(writer: String!, description : String!):String!
+}
+type Subscription {
+  newChat: Chat
+}
 `;
-
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`
+    chatting: () => {
+      return chattingLog;
+    }
+  },
+  Mutation : {
+    write : (_, {writer, description}) =>{
+      const id = chattingLog.length;
+      const newChat = {
+        id,
+        writer,
+        description
+      };
+      chattingLog.push(newChat); //소스 상단의 채팅 배열(날아감) -> 추후 prisma 추가 필요
+      pubsub.publish(NEW_CHAT,{
+        newChat
+      })
+      return "YES";
+    } 
+  },
+  Subscription : {
+    newChat : {
+      subscribe : (_, __, {pubsub}) =>
+      pubsub.asyncIterator(NEW_CHAT)
+    }
   }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-// const server = new GraphQLServer({ schema }); 
-server.start(() => console.log("Server is running on localhost:4000"));
+const server = new GraphQLServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  context: { pubsub }
+});
+
+server.start(() => console.log("Graphql Server Running"));
